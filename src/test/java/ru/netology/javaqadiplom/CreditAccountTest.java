9d16d5cf28f34eb55a376f1d456a06b216494908
Nikita <inik8808@gmail.com>
2023-05-30 03:00:08 +0500

    added tests to SavingAccountTest

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -8,13 +8,12 @@ public class CreditAccountTest {
     @Test
     public void shouldAddToPositiveBalance() {
         CreditAccount account = new CreditAccount(
-                0,
+                200,
                 5_000,
                 15
         );
 
-        account.add(3_000);
 
-        Assertions.assertEquals(3_000, account.getBalance());
+        Assertions.assertEquals(30, account.yearChange());
     }
 }
